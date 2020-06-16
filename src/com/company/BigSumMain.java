75090dbd01517737177e20b55582a7f146c7520b
sunjialin <sunjialin@rong360.com>
2020-06-16 10:10:41 +0800

    big sum big multiply

--- src/com/company/BigSumMain.java
+++ src/com/company/BigSumMain.java
@@ -1,44 +1,29 @@
 package com.company;
 
-import java.util.*;
-
 public class BigSumMain {
     public static void main(String[] args) {
         String a = sum("1017", "3028");
         System.out.println(a);
     }
 
-    static String sum(String a, String b) {
-        if (a == null || b == null || a.length() == 0 || b.length() == 0) {
-            return "";
+    static String sum(String num1, String num2) {
+        StringBuilder res = new StringBuilder();
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
+        int carry = 0;
+        while (i >= 0 || j >= 0) {
+            int n1 = i >= 0 ? num1.charAt(i) - '0' : 0;
+            int n2 = j >= 0 ? num2.charAt(j) - '0' : 0;
+            int tmp = n1 + n2 + carry;
+            carry = tmp /10;
+            res.append(tmp % 10);
+            i--;
+            j--;
+        }
+        if (carry == 1){
+            res.append(1);
         }
 
-        int aLen = a.length();
-        int bLen = b.length();
-        int maxLen = Math.max(a.length(), b.length()) + 1;
-        char[] res = new char[maxLen];
-        Arrays.fill(res, '0');
-
-        for (int i = 1; i < maxLen; i++) {
-            int temp = (res[maxLen - i] - '0');
-            if (aLen >= i) {
-                temp += (a.charAt(aLen - i) - '0');
-            }
-            if (bLen >= i) {
-                temp += (b.charAt(bLen - i) - '0');
-            }
-
-            int c = temp % 10 + '0';
-            int d = temp / 10 + '0';
-
-            res[maxLen - i] = (char)c;
-            res[maxLen - i - 1] = (char)d;
-        }
-
-        if (res[0] == '0') {
-            return String.valueOf(res, 1, maxLen - 1);
-        } else {
-            return String.valueOf(res, 0, maxLen);
-        }
+        return res.reverse().toString();
     }
 }
